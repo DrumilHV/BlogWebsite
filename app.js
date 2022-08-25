1b51e177f5343c589aee08001c4d7724992600cf
drumil <drumilhv@gmail.com>
2022-08-25 21:22:32 +0530

    This is after deleting the redux package which was added for some random error

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require('lodash');
-const { compose } = require("@reduxjs/toolkit");
+
 const mongoose = require("mongoose");
 
 //connecting the mongodb server local as of now 
